c73dc93339e62ccd83e74ae197843c91642526ae
hpeacher <hpeacher@iastate.edu>
2025-05-15 18:41:04 -0500

    maybe fixed

--- terraform/app-deploy.tf
+++ terraform/app-deploy.tf
@@ -8,26 +8,7 @@ resource "null_resource" "app_service" {
     }
 
     inline = [
-      <<-EOT
-        sudo bash -c 'cat <<EOF > /etc/systemd/system/myapp.service
-[Unit]
-Description=My Python App
-After=network.target
-
-[Service]
-ExecStartPre=/bin/bash -c "[ -f /opt/app/app.py ] || git clone https://github.com/hpeacher/422FinalProject.git /opt/app"
-ExecStartPre=/bin/bash -c "/usr/bin/python3 -m venv /opt/app/venv && /opt/app/venv/bin/python -m ensurepip --upgrade"
-ExecStartPre=/opt/app/venv/bin/pip install --no-cache-dir -r /opt/app/requirements.txt
-WorkingDirectory=/opt/app
-ExecStart=/opt/app/venv/bin/python /opt/app/app.py
-Restart=always
-User=ubuntu
-
-[Install]
-WantedBy=multi-user.target
-EOF'
-      EOT,
-      "sudo systemctl daemon-reexec",
+      "sudo bash -c 'cat <<EOF > /etc/systemd/system/myapp.service\n[Unit]\nDescription=My Python App\nAfter=network.target\n\n[Service]\nExecStartPre=/bin/bash -c \"[ -f /opt/app/app.py ] || git clone https://github.com/hpeacher/422FinalProject.git /opt/app\"\nExecStartPre=/usr/bin/python3 -m venv /opt/app/venv\nExecStartPre=/opt/app/venv/bin/python -m ensurepip --upgrade\nExecStartPre=/opt/app/venv/bin/pip install --no-cache-dir -r /opt/app/requirements.txt\nWorkingDirectory=/opt/app\nExecStart=/opt/app/venv/bin/python /opt/app/app.py\nRestart=always\nUser=ubuntu\n\n[Install]\nWantedBy=multi-user.target\nEOF'",
       "sudo systemctl daemon-reload",
       "sudo systemctl enable myapp",
       "sudo systemctl start myapp"
