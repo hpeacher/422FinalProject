96a747c1ace7642f6267b895ae9e6c9b7bfea2c7
hpeacher <hpeacher@iastate.edu>
2025-05-15 17:46:54 -0500

    Trying to fix dependency errors

--- terraform/scripts/cloud-init.sh
+++ terraform/scripts/cloud-init.sh
@@ -3,13 +3,14 @@
 # Install dependencies
 apt update
 apt install -y python3-pip
+apt install -y git
 
 # Clone your app or unpack it from storage
 git clone https://github.com/hpeacher/422FinalProject.git /opt/app
 
 # Optional: install Python dependencies
-cd /opt/app
-pip3 install -r requirements.txt
+#cd /opt/app
+#pip3 install -r requirements.txt
 
 # Create a systemd service to auto-start app
 cat <<EOF > /etc/systemd/system/myapp.service
@@ -20,6 +21,7 @@ After=network.target
 [Service]
 ExecStartPre=/usr/bin/mkdir -p /opt/app
 ExecStartPre=/bin/bash -c '[ -f /opt/app/app.py ] || git clone https://github.com/hpeacher/422FinalProject.git /opt/app'
+ExecStartPre=/usr/bin/pip3 install -r /opt/app/requirements.txt
 User=root
 WorkingDirectory=/
 ExecStart=/usr/bin/python3 /opt/app/app.py
