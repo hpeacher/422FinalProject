20dcd50e264a3eeee2623d1add1bda8a1400e432
hpeacher <hpeacher@iastate.edu>
2025-05-15 04:15:39 -0500

    Auto deployment updated

---
+++ scripts/cloud-init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Install dependencies
+apt update
+apt install -y python3-pip
+
+# Clone your app or unpack it from storage
+git clone https://github.com/hpeacher/422FinalProject.git /opt/app
+
+# Optional: install Python dependencies
+cd /opt/app
+pip3 install -r requirements.txt
+
+# Create a systemd service to auto-start app
+cat <<EOF > /etc/systemd/system/myapp.service
+[Unit]
+Description=Start My App on Boot
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=/opt/app
+ExecStart=/usr/bin/python3 /opt/app/main.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable the service
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable myapp
+systemctl start myapp
---
+++ terraform/app-deploy.tf
@@ -0,0 +1,7 @@
+resource "google_compute_instance" "app_instance" {
+  ...
+
+  metadata_startup_script = file("scripts/cloud-init.sh")
+
+  tags = ["http-server"]
+}
